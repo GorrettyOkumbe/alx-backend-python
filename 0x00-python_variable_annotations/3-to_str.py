347bb03b546c2c9cef8364d0ef16e0b0794271fc
GorrettyOkumbe <okumbegorretty4@gmail.com>
2023-01-05 22:48:49 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" float converted to string """
+
+
+def to_str(n: float) -> str:
+    """
+    to_str: converts float to string
+    n: floating point number
+    Returns:
+        n as str
+    """
+    return str(n)
