ffb4fd6c43d4fbf24883dbbaca4a792b1b14cc9e
GorrettyOkumbe <okumbegorretty4@gmail.com>
2023-01-05 22:40:51 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" floor module """
+
+
+def floor(n: float) -> int:
+    """
+    Takes floating point number
+    n: float number
+    Returns :
+        interger of the floating point number
+    """
+    return int(n)
