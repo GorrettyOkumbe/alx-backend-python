113161309987ee08b72ab4105f4466b52b184828
GorrettyOkumbe <okumbegorretty4@gmail.com>
2023-01-10 01:13:25 +0300

    regular code

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,7 +7,7 @@ from asyncio import Task, create_task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Task:
     """ A synchronous(regular function)"""
     task_1 = create_task(wait_random(max_delay))
     return task_1
