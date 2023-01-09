9ed6a8c3f83e1033d8c9c980b405cac33cfb12f2
GorrettyOkumbe <okumbegorretty4@gmail.com>
2023-01-10 01:10:26 +0300

    regular code

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Synchronous code"""
+
+
+from asyncio import Task, create_task
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """ A synchronous(regular function)"""
+    task_1 = create_task(wait_random(max_delay))
+    return task_1
